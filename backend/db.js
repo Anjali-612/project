6bd0e6da929914741adfb306d8d9cb4eac00fa05
vybhavi muktevi <vybhavimuktevi6@gmail.com>
2026-05-23 17:30:28 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,20 @@
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL || 'postgresql://postgres:postgres12345@localhost:5433/prajwala',
+// });
+
+// pool.on('error', (err) => {
+//   console.error('Database pool error:', err);
+// });
+
+// module.exports = pool;
 const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL || 'postgresql://postgres:postgres12345@localhost:5433/prajwala',
+  // 👇 ADD THIS SSL BLOCK HERE
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
 pool.on('error', (err) => {
